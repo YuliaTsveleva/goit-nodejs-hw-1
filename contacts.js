8bf9f40d163daac29d59035716502007782ecc67
YuliaTsveleva <tsveleva.yulia@gmail.com>
2022-02-12 04:17:15 +0200

    add functions

--- contacts.js
+++ contacts.js
@@ -1,3 +1,69 @@
 const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
 
-// const fs = require("fs").promises;   /*вариант записи*/
+const contactsPath = path.join(__dirname, "db/contacts.json");
+// console.log(__dirname);
+
+// TODO: задокументировать каждую функцию
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  return contacts;
+};
+
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === contactId);
+  if (!result) {
+    return null;
+  }
+  return result;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = await findIndexOfContactById(contacts, contactId);
+  if (index === -1) {
+    return null;
+  }
+  const removedContact = contacts.splice(index, 1);
+  updateContacts(contacts);
+  return removedContact;
+};
+
+const addContact = async ({ name, email, phone }) => {
+  const contacts = await listContacts();
+  const newContact = { name, email, phone, id: nanoid() };
+  contacts.push(newContact);
+  updateContacts(contacts);
+  return newContact;
+};
+
+const updateContactById = async (id, data) => {
+  const contacts = await listContacts();
+  const index = await findIndexOfContactById(contacts, id);
+  if (index === -1) {
+    return null;
+  }
+  contacts[index] = { ...data, id };
+  updateContacts(contacts);
+  return contacts[index];
+};
+
+const updateContacts = async (contacts) => {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+};
+
+const findIndexOfContactById = async (contacts, id) => {
+  const indexOfContact = await contacts.findIndex((item) => item.id === id);
+  return indexOfContact;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContactById,
+};
--- index.js
+++ index.js
@@ -1,1 +1,62 @@
-console.log("It's my first project");
+const contactsOperations = require("./contacts");
+// const argv = require("yargs").argv;
+
+// TODO: рефакторить
+async function invokeAction({ action, id, data }) {
+  switch (action) {
+    case "list":
+      const contacts = await contactsOperations.listContacts();
+      console.log(contacts);
+      break;
+
+    case "get":
+      const contact = await contactsOperations.getContactById(id);
+      if (!contact) {
+        throw new Error(`Contact witn ${id} not found`);
+      }
+      console.log(contact);
+      break;
+
+    case "add":
+      const newContact = await contactsOperations.addContact(data);
+      console.log(newContact);
+      break;
+
+    case "update":
+      const updatedContact = await contactsOperations.updateContactById(
+        id,
+        data
+      );
+      if (!updatedContact) {
+        throw new Error(`Contact witn ${id} not found`);
+      }
+      console.log(updatedContact);
+      break;
+
+    case "remove":
+      const removedContact = await contactsOperations.removeContact(id);
+      console.log(removedContact);
+      break;
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+// invokeAction(argv);
+
+// const newData = {
+//   name: "Sherlock Holmes",
+//   email: "baker-street@gmail.com",
+//   phone: "(111) 111-1111",
+// };
+
+// const updateId = "Dw-8nhp-R24AhF3qp6lYi";
+// const updatedData = {
+//   name: "Sherlock Holmes",
+//   email: "baker-street-221b@gmail.com",
+//   phone: "(222) 222-2222",
+// };
+
+const removeId = "Dw-8nhp-R24AhF3qp6lYi";
+
+invokeAction({ action: "remove", id: removeId });
